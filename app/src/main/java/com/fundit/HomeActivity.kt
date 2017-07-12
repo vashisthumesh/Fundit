56ad42ff22386884ef4e3c7b6bd57b41ed0d0681
chintan369 <chintak369@gmail.com>
2017-07-12 16:03:26 +0530

    Starting of Project

--- app/src/main/java/com/fundit/HomeActivity.kt
+++ app/src/main/java/com/fundit/HomeActivity.kt
@@ -2,9 +2,13 @@ package com.fundit
 
 import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentManager
+import android.support.v4.app.FragmentTransaction
 import android.support.v7.app.AppCompatActivity
 import android.widget.TextView
 import com.fundit.a.C
+import com.fundit.fragmet.MyCampaignsFragment
 
 class HomeActivity : AppCompatActivity() {
 
@@ -21,7 +25,7 @@ class HomeActivity : AppCompatActivity() {
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_myCampaigns -> {
-                mTextMessage!!.text = item.title
+                setMyCampaignFragment()
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_browse -> {
@@ -30,6 +34,14 @@ class HomeActivity : AppCompatActivity() {
             }
         }
         false
+    }
+
+    private fun setMyCampaignFragment() {
+        val fragment:Fragment = MyCampaignsFragment()
+        val fm: FragmentManager = supportFragmentManager
+        val transaction: FragmentTransaction = fm.beginTransaction()
+        transaction.replace(R.id.content,fragment)
+        transaction.commit()
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
