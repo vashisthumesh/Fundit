dad843a44535c294149f4fe8c907f7b758ad7236
umesh <vashisthumesh@gmail.com>
2017-07-13 17:06:45 +0530

    screens from umesh

---
+++ app/src/main/java/com/fundit/FundSpotProfile.java
@@ -0,0 +1,13 @@
+package com.fundit;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FundSpotProfile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fund_spot_profile);
+    }
+}
