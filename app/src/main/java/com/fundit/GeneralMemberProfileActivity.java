07ab0f3d2099b0baabdccf191f29cb2ea8a81f04
chintan369 <chintak369@gmail.com>
2017-07-17 15:23:39 +0530

    Starting of Project

--- app/src/main/java/com/fundit/GeneralMemberProfileActivity.java
+++ app/src/main/java/com/fundit/GeneralMemberProfileActivity.java
@@ -2,16 +2,35 @@ package com.fundit;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Spinner;
 
 import com.fundit.a.C;
+import com.fundit.apis.AdminAPI;
+import com.fundit.apis.ServiceGenerator;
 
 public class GeneralMemberProfileActivity extends AppCompatActivity {
+
+    EditText edt_firstName,edt_lastName,edt_assocOrganization,edt_assocFundspot,edt_contactInfo;
+    ImageView img_profilePic;
+    Button btn_updateProfile;
+    Spinner spn_state, spn_city;
+    AdminAPI adminAPI;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_general_member_profile);
 
+        adminAPI= ServiceGenerator.getAPIClass();
+
+        fetchIDs();
+
+    }
+
+    private void fetchIDs() {
 
     }
 }
