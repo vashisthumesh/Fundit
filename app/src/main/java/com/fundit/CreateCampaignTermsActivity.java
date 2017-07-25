0ea7a39dca7e12b7f72a2d9eb9b8175f7d6b4c7c
chintan369 <chintak369@gmail.com>
2017-07-25 14:25:28 +0530

    Integration of Webservices

---
+++ app/src/main/java/com/fundit/CreateCampaignTermsActivity.java
@@ -0,0 +1,27 @@
+package com.fundit;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+
+public class CreateCampaignTermsActivity extends AppCompatActivity {
+
+    TextView txt_partnerLabel, txt_itemLabel;
+    EditText edt_partner, edt_itemName, edt_price, edt_split, edt_campaignDuration, edt_maxLimitCoupon, edt_couponExpireDay, edt_finePrint;
+    RadioGroup rg_productType;
+    RadioButton rdo_typeItem, rdo_typeGiftCard;
+    CheckBox chk_infinite;
+    Button btn_accept, btn_message, btn_decline;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_campaign_terms);
+    }
+}
--- app/src/main/java/com/fundit/organization/CreateCampaignNextActivity.java
+++ app/src/main/java/com/fundit/organization/CreateCampaignNextActivity.java
@@ -4,12 +4,27 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 import com.fundit.R;
+import com.fundit.a.AppPreference;
+import com.fundit.apis.AdminAPI;
+import com.fundit.apis.ServiceGenerator;
 
 public class CreateCampaignNextActivity extends AppCompatActivity {
+
+    AdminAPI adminAPI;
+    AppPreference preference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_campaign_next);
+
+        preference = new AppPreference(this);
+        adminAPI = ServiceGenerator.getAPIClass();
+
+        fetchIDs();
+    }
+
+    private void fetchIDs() {
+
     }
 }
