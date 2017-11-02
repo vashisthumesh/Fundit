b7b58c9e3654ab071de637a2382b5e93908a8d61
Keval Shukla <kevals02@outlook.com>
2017-11-02 11:22:35 +0530

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	app/src/main/java/com/fundit/FundSpotProfile.java
    #	app/src/main/java/com/fundit/organization/FundspotProductListActivity.java

--- app/src/main/java/com/fundit/FinalOrderPlace.java
+++ app/src/main/java/com/fundit/FinalOrderPlace.java
@@ -191,7 +191,7 @@ public class FinalOrderPlace extends AppCompatActivity  {
 
         }
 
-        Log.e("testKeval" , "--->test");
+        Log.e("testKeval" , "--->test" +C.FUNDSPOT);
         if (preference.getUserRoleID().equalsIgnoreCase(C.FUNDSPOT)) {
             txt_partnerTitle.setText("Organization :");
             if (campaignList.getCampaign().getReview_status().equals(1)) {
